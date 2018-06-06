809084f133d4dd3d484251763c6db6cf94f4e991
Gionaldo Hartono <Gionaldo@Gionaldos-MacBook-Pro.local>
2018-06-06 05:59:51 -0700

    fix try

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 
-//change innerhtml of content
+
+/*//change innerhtml of content
 function changeTemp(template) {
   if (template === "")
     template = "home"
 
   var temp = '[data-template='+template+'-page]'
+  const node = findNode('[id=content')
   if (!findNode(temp))
-    document.getElementById("content").innerHTML = document.querySelector('[data-template=error404-page]').innerHTML
+    render('error404', node)
   else
     document.getElementById("content").innerHTML = document.querySelector(temp).innerHTML
 
@@ -14,9 +16,39 @@ function changeTemp(template) {
 
 //on window load, get the url
 window.onload = function(){
+
+
+  onBrowse(
+}
+
+var node = findNode('[id=content]')
+loadNodeSource(node)
+var pathname = window.location.pathname
+var template = pathname.substr(1)
+function onBrowse(state, node, err, text,xhr)
+{
+  if (state != finish) return;
+  var obj = {
+    error: err,
+    results: err ? undefined : JSON.parse(text)
+  }
+  render(template,obj,node);
+  }
+}
+
+*/
+
+document.addEventListener("DOMContentLoaded", (e) => {
   var pathname = window.location.pathname
   var template = pathname.substr(1)
+  var node = findNode('[id=content]')
 
+  if (template === "")
+    template = "home"
 
-  changeTemp(template)
-} 
+  var temp = '[data-template='+template+'-page]'  
+  if (!findNode(temp))
+    document.getElementById("content").innerHTML = document.querySelector('[data-template=error404-page]').innerHTML
+  else
+    document.getElementById("content").innerHTML = document.querySelector(temp).innerHTML
+})
