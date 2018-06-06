ade57e1f28ddb48927a290e8b417a11481bf5f93
Gionaldo Hartono <Gionaldo@Gionaldos-MacBook-Pro.local>
2018-06-06 06:41:23 -0700

    heroku deploy

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,32 @@
 
 
-/*//change innerhtml of content
-function changeTemp(template) {
-  if (template === "")
-    template = "home"
-
-  var temp = '[data-template='+template+'-page]'
-  const node = findNode('[id=content')
-  if (!findNode(temp))
-    render('error404', node)
-  else
-    document.getElementById("content").innerHTML = document.querySelector(temp).innerHTML
-
-}
-
-//on window load, get the url
-window.onload = function(){
-
-
-  onBrowse(
-}
-
-var node = findNode('[id=content]')
-loadNodeSource(node)
-var pathname = window.location.pathname
-var template = pathname.substr(1)
-function onBrowse(state, node, err, text,xhr)
-{
-  if (state != finish) return;
-  var obj = {
-    error: err,
-    results: err ? undefined : JSON.parse(text)
-  }
-  render(template,obj,node);
-  }
-}
-
-*/
-
 document.addEventListener("DOMContentLoaded", (e) => {
   var pathname = window.location.pathname
   var template = pathname.substr(1)
-  var node = findNode('[id=content]')
-
+  var id = "[id=content]"
+  var node = findNode(id)
+  renderContent(temp)
+  /*
   if (template === "")
     template = "home"
 
   var temp = '[data-template='+template+'-page]'  
   if (!findNode(temp))
-    document.getElementById("content").innerHTML = document.querySelector('[data-template=error404-page]').innerHTML
+    document.querySelector(id).innerHTML = document.querySelector('[data-template=error404-page]').innerHTML
   else
-    document.getElementById("content").innerHTML = document.querySelector(temp).innerHTML
+    document.querySelector(id).innerHTML = document.querySelector(temp).innerHTML
+  */
+
 })
+
+function renderContent (template) {
+  const node = findNode('[id=content]')
+
+  betterRender(template, node)
+}
+
+function betterRender (template, node, scope) {
+  const el = render(template, scope, node)
+
+  loadNodeSources(el)
+}
